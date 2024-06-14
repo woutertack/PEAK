7deaa455fcb7df953f29e695737e5899404f4f4c
woutertack <wouttack1@student.arteveldehs.be>
2024-06-14 15:25:15 +0200

    Add eas.json to .gitignore and remove from tracking

---
+++ src/screens/Test.js
@@ -0,0 +1,48 @@
+import React, { useRef, useEffect } from 'react';
+import { Button, StyleSheet, View } from 'react-native';
+import LottieView from 'lottie-react-native';
+
+export default function Test() {
+  const animation = useRef(null);
+  useEffect(() => {
+    // You can control the ref programmatically, rather than using autoPlay
+    // animation.current?.play();
+  }, []);
+
+  return (
+    <View style={styles.animationContainer}>
+      <LottieView
+        autoPlay
+        ref={animation}
+        style={{
+          width: 200,
+          height: 200,
+          backgroundColor: '#eee',
+        }}
+
+        source={require('./../components/utils/animations/confetti.json')}
+      />
+      <View style={styles.buttonContainer}>
+        <Button
+          title="Restart Animation"
+          onPress={() => {
+            animation.current?.reset();
+            animation.current?.play();
+          }}
+        />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  animationContainer: {
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
+  },
+  buttonContainer: {
+    paddingTop: 20,
+  },
+});
